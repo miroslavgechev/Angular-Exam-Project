de11d74ee9aaf2e31df2dcb2adfc4e0e223bc76b
Miroslav Gechev <vindaloo89@gmail.com>
2023-07-28 18:18:44 +0300

    Not found page added

--- store-app/src/app/app-routing.module.ts
+++ store-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '',pathMatch: 'full', redirectTo: 'catalog'},
-  {path: '**', redirectTo: 'catalog'}
+  {path: '404', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '404'}
 
 ];
 
